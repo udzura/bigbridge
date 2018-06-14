7aebb138927f26592e6b6842bd7714e9e2332bc8
Uchio KONDO <udzura@pepabo.com>
2018-06-14 20:02:11 +0900

    Implement attacker

--- attacker/main.go
+++ attacker/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gopkg.in/headzoo/surf.v1"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, attacker\n")
+	bow := surf.NewBrowser()
+	err := bow.Open("http://localhost:18080/app.php")
+	if err != nil {
+		panic(err)
+	}
+
+	err = bow.Open("http://localhost:18080/app.php?name=udzura<script>alert(1);</script>")
+	if err != nil {
+		panic(err)
+	}
+	found := bow.Find("script").Size()
+	fmt.Printf("Script element size: %v\n", found)
+	if found == 0 {
+		fmt.Printf("Defence success!!! Omedetou!!!!!!!!!!!!\n")
+	} else {
+		fmt.Printf("Attack success!!!\n")
+		os.Exit(1)
+	}
+
 }
