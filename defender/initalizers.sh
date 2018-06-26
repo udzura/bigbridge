eb7708a462a7993502aa76356ef5571d7dc4d19d
Uchio KONDO <udzura@pepabo.com>
2018-06-26 19:00:37 +0900

    Complete setup

--- defender/initalizers.sh
+++ defender/initalizers.sh
@@ -6,15 +6,21 @@ mkdir /var/run/mysqld
 chmod 777 /var/run/mysqld
 chmod 700 /run/sshd
 
-echo -n '[mysqld]\nskip-grant-tables' > /etc/mysql/conf.d/setup.cnf
+rm -rf /var/lib/mysql/*
+install -d -o mysql -g mysql -m 750 /var/lib/mysql/{tmp,ibdata,iblog}
+/usr/sbin/mysqld --initialize --user=mysql --ignore-db-dir=tmp --ignore-db-dir=ibdata --ignore-db-dir=iblog
 
-# mysqld_safe &
-# until echo -e "root\n" | nc localhost 3306 >/dev/null; do
-#     sleep 0.1
-# done
+rootpass=$(grep 'A temporary password is generated for root@localhost:' /var/log/mysql/* | awk '{print $NF}')
+mysqld_safe &
+until echo -e "root\n" | nc localhost 3306 >/dev/null; do
+  sleep 0.1
+done
 
-# #mysql_upgrade
-# echo 'create database testapp' | mysql -uroot -proot
-# echo 'create table messages (id int auto_increment not null primary key, username varchar(255), message text);' | mysql -uroot -proot testapp
-# kill $(cat /var/run/mysqld/mysqld.pid)
-# rm -f /etc/mysql/conf.d/setup.cnf
+echo "alter user root@localhost identified by 'enpit-pr0@0630'" | mysql -uroot -p$rootpass --connect-expired-password
+rootpass='enpit-pr0@0630'
+
+echo 'create database testapp;' | mysql -uroot -p$rootpass
+echo "create user bigbridge@localhost identified by 'bigbridge0630';" | mysql -uroot -p$rootpass
+echo "grant all on testapp.* to bigbridge@localhost identified by 'bigbridge0630';" | mysql -uroot -p$rootpass
+echo 'create table messages (id int auto_increment not null primary key, username varchar(255), message text);' | mysql -uroot -p$rootpass testapp
+kill $(cat /var/run/mysqld/mysqld.pid)
